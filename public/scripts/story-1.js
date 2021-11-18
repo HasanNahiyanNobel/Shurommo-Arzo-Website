6b86031d70f4b2cbd984a37edf4bfbbcef4d7c57
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-11-18 09:42:19 +0600

    Implemented a spinner until the image is fully loaded.

--- public/scripts/story-1.js
+++ public/scripts/story-1.js
@@ -1,32 +1,49 @@
-const body = document.body;
-// const html = document.documentElement;
+// JS to show spinner until the image is loaded.
+let theImage = document.getElementById(`graphics-main-shahorik`);
+let theDiv = document.getElementById(`oteeddho-unloaded-div`);
+let theSpinner = document.getElementById(`oteeddho-spinner`);
 
-const navbar = document.getElementById(`main-navbar`);
-// const navWrapper = document.getElementById(`main-nav-wrapper`);
-const graphics = document.getElementById(`graphics-main-shahorik`);
-
-// const pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight); // বিপদের দিনে এই জিনিস হয়তো সাহায্য করতে পারে। :3
-// const pageHeight = body.offsetHeight;
-// const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0); // Taken from: https://stackoverflow.com/a/8876069
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0); // Also from: https://stackoverflow.com/a/8876069
-const navbarHeight = navbar.offsetHeight;
-
-// const graphicsStartPos = graphics.offsetTop - navbarHeight;
-// const graphicsHeight = graphics.offsetHeight;
-// console.log(`Graphics start: ${graphicsStartPos}`);
-// console.log(`Graphics end: ${graphicsStartPos + graphicsHeight}`);
-// console.log(`Page height: ${body.offsetHeight}`);
-
-window.onscroll = function() {
-	let graphicsPos = graphics.getBoundingClientRect();
-	let currentTop = Math.round(graphicsPos.top) - navbarHeight;
-	// let currentBottom = Math.round(graphicsPos.bottom) - navbarHeight;
-	// console.log(`Current: ${currentTop}, ${currentBottom}`);
-
-	if (currentTop < vh / 2) { // When the image comes halfway through the viewport, we see the colour-change
-		navbar.style.backgroundColor = `#fbc02d`; // yellow darken-2
-		body.style.backgroundColor = `#fff9c4`; // yellow lighten-4
+let interval = setInterval(() => {
+	if (theImage.complete) {
+		theSpinner.classList.add(`d-none`);
+		theDiv.classList.remove(`d-none`);
+		clearInterval(interval);
+		handler();
 	}
-};
+}, 200);
 
-// window.onscroll(undefined);
+// The handler function after the image is loaded.
+function handler () {
+	const body = document.body;
+	// const html = document.documentElement;
+
+	const navbar = document.getElementById(`main-navbar`);
+	// const navWrapper = document.getElementById(`main-nav-wrapper`);
+	const graphics = document.getElementById(`graphics-main-shahorik`);
+
+	// const pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight); // বিপদের দিনে এই জিনিস হয়তো সাহায্য করতে পারে। :3
+	// const pageHeight = body.offsetHeight;
+	// const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0); // Taken from: https://stackoverflow.com/a/8876069
+	const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0); // Also from: https://stackoverflow.com/a/8876069
+	const navbarHeight = navbar.offsetHeight;
+
+	// const graphicsStartPos = graphics.offsetTop - navbarHeight;
+	// const graphicsHeight = graphics.offsetHeight;
+	// console.log(`Graphics start: ${graphicsStartPos}`);
+	// console.log(`Graphics end: ${graphicsStartPos + graphicsHeight}`);
+	// console.log(`Page height: ${body.offsetHeight}`);
+
+	window.onscroll = function() {
+		let graphicsPos = graphics.getBoundingClientRect();
+		let currentTop = Math.round(graphicsPos.top) - navbarHeight;
+		// let currentBottom = Math.round(graphicsPos.bottom) - navbarHeight;
+		// console.log(`Current: ${currentTop}, ${currentBottom}`);
+
+		if (currentTop < vh / 2) { // When the image comes halfway through the viewport, we see the colour-change
+			navbar.style.backgroundColor = `#fbc02d`; // yellow darken-2
+			body.style.backgroundColor = `#fff9c4`; // yellow lighten-4
+		}
+	};
+
+	// window.onscroll(undefined);
+}
