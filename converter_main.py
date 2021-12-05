a001b21ec149828fc8895032b69e68ba31ab4552
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-05 17:44:17 +0600

    Added a basic converter.

---
+++ converter_main.py
@@ -0,0 +1,82 @@
+#
+# This converter generates static html files from a base template and source files (a bit like Django!)
+#
+
+import os.path
+from os import listdir
+from os.path import isfile, join
+from utilities import *
+
+# Variables
+pre_content = []
+content = []
+post_content = []
+
+# Read pre- and post-contents from base file
+with open(BASE_FILE, encoding='utf-8') as f:
+    for line in f:
+        if line.strip() == CONTENT_BLOCK_MARKER:
+            break
+        pre_content.append(line)
+
+    for line in f:
+        post_content.append(line)
+
+# Make a list of the source files. Concept from: https://stackoverflow.com/a/3207973.
+source_files = [
+    f for f in listdir(SOURCE_DIR)
+    if isfile(join(SOURCE_DIR, f)) and f[0] != '_'
+]  # Files starting with an underscore are template files, so they are not included in this list.
+
+for source_file in source_files:
+    # Read the source file
+    source_path = SOURCE_DIR + '\\' + source_file
+    with open(source_path, encoding='utf-8') as f:
+        for line in f:
+            current_line = line.strip()
+            if current_line == START_OF_CONTENT_BLOCK_MARKER:
+                content = read_block(f)
+
+    # Create modified pre- and post-contents (to give the current page an `active` class in navbar)
+    modified_pre_content = [
+        sub.replace('{{ ' + source_file + ' }}', REPLACED_ACTIVE_CLASS)
+        for sub in pre_content
+    ]
+    modified_post_content = [
+        sub.replace('{{ ' + source_file + ' }}', REPLACED_ACTIVE_CLASS)
+        for sub in post_content
+    ]
+
+    # Write the contents in respective output files
+    output_path = OUTPUT_DIR + '\\' + source_file
+    with open(
+            output_path, 'w+', encoding='utf-8'
+    ) as f:  # The 'w+' option creates the file if it does not exist already
+        f.writelines(modified_pre_content + content + modified_post_content)
+
+    # Clean the arrays
+    fluid_content = []
+    content = []
+
+# Make a list of the output files
+output_files = [
+    f for f in listdir(OUTPUT_DIR)
+    if isfile(join(OUTPUT_DIR, f)) and os.path.splitext(f)[1] == '.html'
+]  # Output directory may contain files other than html—so this extra check is included in if-condition.
+
+# Remove the output files which have been deleted from source files
+for output_file in output_files:
+    if not source_files.__contains__(output_file):
+        os.remove(OUTPUT_DIR + '\\' +
+                  output_file)  # Remove the file from directory
+        output_files.remove(output_file)  # Also remove the file from list
+        print('Deleted: ' + output_file)
+
+# Minify the script using html-minifier (https://www.npmjs.com/package/html-minifier)
+os.system(
+    'html-minifier --collapse-whitespace --minify-js true --no-html5 --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --input-dir public_beta --output-dir public_beta --file-ext html'
+)  # TODO: Change input and output dir here
+
+# Add copyright lines to the minified files
+for output_file in output_files:
+    prepend_line(OUTPUT_DIR + '\\' + output_file, COPYRIGHT_LINE)
---
+++ utilities.py
@@ -0,0 +1,80 @@
+"""
+This script contains some constants and functions used by the other scripts.
+Copyright © 2021 by Oteeddho.
+"""
+
+#
+# Constants
+#
+SOURCE_DIR = 'src_html'
+OUTPUT_DIR = 'public_beta'
+BASE_FILE = SOURCE_DIR + '\\_base.html'
+
+DATA_BLOCK_MARKER = '{% block data %}'
+CONTENT_BLOCK_MARKER = '{% block content %}{% endblock %}'
+START_OF_CONTENT_BLOCK_MARKER = '{% block content %}'
+END_OF_BLOCK_MARKER = '{% endblock %}'
+
+REPLACED_ACTIVE_CLASS = 'fw-bold active'
+
+COPYRIGHT_LINE = '<!--\n---- এখানে কী চাই? 🤨\n---- এই ফাইলটা পাইথন ফ্রেমওয়ার্ক দিয়ে নির্মিত, এবং এইচটিএমএল মিনিফায়ার দিয়ে ক্ষুদ্রীকৃত।\n---- © ২০২১, সুরম্য আর্য।\n--->'
+
+
+
+def prepend_line (filepath, line_to_prepend):
+    """
+    Prepend lines at the beginning of a file. Concept from https://stackoverflow.com/a/5917395.
+
+    :param filepath: Path of the file.
+    :param line_to_prepend: The line to prepend.
+    :return: Nothing.
+    """
+    with open(filepath, 'r+', encoding='utf-8') as a_file:
+        content = a_file.read()
+        a_file.seek(0, 0)
+        a_file.write(line_to_prepend.rstrip('\r\n') + '\n' + content)
+
+
+
+# Read content block
+def read_block (file):
+    """
+    Read lines from a file until the end of block marker is read.
+
+    :param file: The file from which the lines are being read.
+    :return: The read lines as an array.
+    """
+    block = []
+    for line in file:
+        if line.strip() == END_OF_BLOCK_MARKER:
+            break
+        block.append(line)
+    return block
+
+
+
+def english_to_bangla_number (number=None):
+    """
+    A slowww, but effective function to convert English numbers to Bangla, as a string.
+
+    :param number: The number to be translated.
+    :return: The translated number in Bangla, as a string. If the input is not parsed as a number, and empty string is returned.
+    """
+    if number is None:
+        # No argument has been passed, return an empty string.
+        return ''
+
+    try:
+        float(number)  # Check whether input is a number.
+        bangla_digits = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯']
+        english_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        number = number.__str__()
+        for i, digit in enumerate(english_digits):
+            number = [sub.replace(digit, bangla_digits[i]) for sub in number]
+        return ''.join(number)
+
+    # Input hasn't been parsed as a number. Return an empty string.
+    except ValueError:
+        return ''
+    except TypeError:
+        return ''
