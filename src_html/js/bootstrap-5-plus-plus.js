e22c780999113cb247f5d96b4fd30240c76a4f7e
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-06 02:22:12 +0600

    Removed unnecessary function calls. Seems to work now.

--- src_html/js/bootstrap-5-plus-plus.js
+++ src_html/js/bootstrap-5-plus-plus.js
@@ -1,24 +1,20 @@
-start();
+let images = document.getElementsByTagName(`img`);
+let unloadedDivs = document.getElementsByClassName(`oteeddho-unloaded-div`);
+let spinner = document.getElementById(`oteeddho-spinner`);
 
-function start() {
-  let images = document.getElementsByTagName(`img`);
-  let unloadedDivs = document.getElementsByClassName(`oteeddho-unloaded-div`);
-  let spinner = document.getElementById(`oteeddho-spinner`);
-
-  let interval = setInterval(() => {
-    if (hasAllTheImagesBeenLoaded()) {
-      for (let div of unloadedDivs) {
-        div.classList.remove(`oteeddho-unloaded-div`);
-      }
-      spinner.classList.add(`oteeddho-unloaded-div`);
-      clearInterval(interval);
+let interval = setInterval(() => {
+  if (hasAllTheImagesBeenLoaded()) {
+    for (let div of unloadedDivs) {
+      div.classList.remove(`oteeddho-unloaded-div`);
     }
-  }, 100);
-
-  function hasAllTheImagesBeenLoaded() {
-    for (let image of images) {
-      if (!image.complete) return false;
-    }
-    return true;
+    spinner.classList.add(`oteeddho-unloaded-div`);
+    clearInterval(interval);
   }
+}, 100);
+
+function hasAllTheImagesBeenLoaded() {
+  for (let image of images) {
+    if (!image.complete) return false;
+  }
+  return true;
 }
--- src_html/js/story-1.js
+++ src_html/js/story-1.js
@@ -1,32 +1,28 @@
-start();
+let body = document.body;
+let navbar = document.getElementsByTagName(`nav`)[0];
+let graphics = document.getElementById(`graphics-main-shahorik`);
+let navbarHeight = navbar.offsetHeight;
+let vh = Math.max(document.documentElement.clientHeight || 0,
+    window.innerHeight || 0); // Taken from: https://stackoverflow.com/a/8876069
 
-function start() {
-  let body = document.body;
-  let navbar = document.getElementsByTagName(`nav`)[0];
-  let graphics = document.getElementById(`graphics-main-shahorik`);
-  let navbarHeight = navbar.offsetHeight;
-  let vh = Math.max(document.documentElement.clientHeight || 0,
-      window.innerHeight || 0); // Taken from: https://stackoverflow.com/a/8876069
+window.onscroll = function() {
+  let graphicsPos = graphics.getBoundingClientRect();
+  let currentTop = Math.round(graphicsPos.top) - navbarHeight;
+  let navbarTransitionTime = `1000ms`;
+  let bodyTransitionTime = `5000ms`;
 
-  window.onscroll = function() {
-    let graphicsPos = graphics.getBoundingClientRect();
-    let currentTop = Math.round(graphicsPos.top) - navbarHeight;
-    let navbarTransitionTime = `1000ms`;
-    let bodyTransitionTime = `5000ms`;
+  // Add transitions
+  navbar.style.transition = `background-color ${navbarTransitionTime} linear`;
+  body.style.transition = `background-color ${bodyTransitionTime} linear`;
 
-    // Add transitions
-    navbar.style.transition = `background-color ${navbarTransitionTime} linear`;
-    body.style.transition = `background-color ${bodyTransitionTime} linear`;
-
-    // When the image comes halfway through the viewport, we see the change of colour
-    if (currentTop < vh / 2) {
-      navbar.classList.remove(`bg-dark`);
-      navbar.style.backgroundColor = `#fbc02d`; // yellow darken-2
-      body.style.backgroundColor = `#fff9c4`; // yellow lighten-4
-    } else {
-      body.style.backgroundColor = ``;
-      navbar.style.backgroundColor = ``;
-      navbar.classList.add(`bg-dark`);
-    }
-  };
-}
+  // When the image comes halfway through the viewport, we see the change of colour
+  if (currentTop < vh / 2) {
+    navbar.classList.remove(`bg-dark`);
+    navbar.style.backgroundColor = `#fbc02d`; // yellow darken-2
+    body.style.backgroundColor = `#fff9c4`; // yellow lighten-4
+  } else {
+    body.style.backgroundColor = ``;
+    navbar.style.backgroundColor = ``;
+    navbar.classList.add(`bg-dark`);
+  }
+};
