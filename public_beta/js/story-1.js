7e65f34c3183a47d2a28aafd1ceec41b96e36e0f
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-05 22:18:24 +0600

    Shahorik seems to work **really** well.

--- public_beta/js/story-1.js
+++ public_beta/js/story-1.js
@@ -1,49 +1,47 @@
-// JS to show spinner until the image is loaded.
-let theImage = document.getElementById(`graphics-main-shahorik`);
-let theDiv = document.getElementById(`oteeddho-unloaded-div`);
-let theSpinner = document.getElementById(`oteeddho-spinner`);
+// JS to show spinner until the image is loaded
+let spinner = document.getElementById(`oteeddho-spinner`);
+let unloadedDivs = document.getElementsByClassName(`oteeddho-unloaded-div`);
+let image = document.getElementById(`graphics-main-shahorik`);
 
 let interval = setInterval(() => {
-  if (theImage.complete) {
-    theSpinner.classList.add(`d-none`);
-    theDiv.classList.remove(`d-none`);
-    clearInterval(interval);
-    handler();
-  }
+    if (image.complete) {
+        for (let aDiv of unloadedDivs) {
+            aDiv.classList.remove(`oteeddho-unloaded-div`);
+        }
+        spinner.classList.add(`oteeddho-unloaded-div`);
+        clearInterval(interval);
+        handler();
+    }
 }, 100);
 
-// The handler function after the image is loaded.
-function handler () {
-  const body = document.body;
-  // const html = document.documentElement;
+// The handler function after the image is loaded
+function handler() {
+    // Define constants
+    const body = document.body;
+    const navbar = document.getElementsByTagName(`nav`)[0];
+    const graphics = document.getElementById(`graphics-main-shahorik`);
+    const navbarHeight = navbar.offsetHeight;
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0); // Taken from: https://stackoverflow.com/a/8876069
+    const navbarTransitionTime = `1000ms`;
+    const bodyTransitionTime = `5000ms`;
 
-  const navbar = document.getElementById(`main-navbar`);
-  // const navWrapper = document.getElementById(`main-nav-wrapper`);
-  const graphics = document.getElementById(`graphics-main-shahorik`);
+    // Add transition style to navbar and body
+    navbar.style.transition = `background-color ${navbarTransitionTime} linear`;
+    body.style.transition = `background-color ${bodyTransitionTime} linear`;
 
-  // const pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight); // বিপদের দিনে এই জিনিস হয়তো সাহায্য করতে পারে। :3
-  // const pageHeight = body.offsetHeight;
-  // const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0); // Taken from: https://stackoverflow.com/a/8876069
-  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0); // Also from: https://stackoverflow.com/a/8876069
-  const navbarHeight = navbar.offsetHeight;
+    window.onscroll = function () {
+        let graphicsPos = graphics.getBoundingClientRect();
+        let currentTop = Math.round(graphicsPos.top) - navbarHeight;
 
-  // const graphicsStartPos = graphics.offsetTop - navbarHeight;
-  // const graphicsHeight = graphics.offsetHeight;
-  // console.log(`Graphics start: ${graphicsStartPos}`);
-  // console.log(`Graphics end: ${graphicsStartPos + graphicsHeight}`);
-  // console.log(`Page height: ${body.offsetHeight}`);
-
-  window.onscroll = function() {
-    let graphicsPos = graphics.getBoundingClientRect();
-    let currentTop = Math.round(graphicsPos.top) - navbarHeight;
-    // let currentBottom = Math.round(graphicsPos.bottom) - navbarHeight;
-    // console.log(`Current: ${currentTop}, ${currentBottom}`);
-
-    if (currentTop < vh / 2) { // When the image comes halfway through the viewport, we see the colour-change
-      navbar.style.backgroundColor = `#fbc02d`; // yellow darken-2
-      body.style.backgroundColor = `#fff9c4`; // yellow lighten-4
-    }
-  };
-
-  // window.onscroll(undefined);
+        // When the image comes halfway through the viewport, we see the change of colour
+        if (currentTop < vh / 2) {
+            navbar.classList.remove(`bg-dark`)
+            navbar.style.backgroundColor = `#fbc02d`; // yellow darken-2
+            body.style.backgroundColor = `#fff9c4`; // yellow lighten-4
+        } else {
+            body.style.backgroundColor = ``;
+            navbar.style.backgroundColor = ``;
+            navbar.classList.add(`bg-dark`);
+        }
+    };
 }
