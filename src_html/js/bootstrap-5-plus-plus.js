a65daa205d5e3fdaf8c1adaf55c5d37a302f04c8
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-07 00:42:45 +0600

    Removed the `disabled` class totally. Using JS to disable image selection and copying, works fine now.

--- src_html/js/bootstrap-5-plus-plus.js
+++ src_html/js/bootstrap-5-plus-plus.js
@@ -1,10 +1,20 @@
 startMain();
 
 function startMain() {
+  // Define variables
   let images = document.getElementsByTagName(`img`);
   let mainDiv = document.getElementById(`md`);
   let spinner = document.getElementById(`ms`);
 
+  // Function to check whether all the images has been loaded
+  function hasAllTheImagesBeenLoaded() {
+    for (let image of images) {
+      if (!image.complete) return false;
+    }
+    return true;
+  }
+
+  // Schedule the interval
   let interval = setInterval(() => {
     if (hasAllTheImagesBeenLoaded()) {
       spinner.classList.add(`d-none`);
@@ -13,11 +23,8 @@ function startMain() {
     }
   }, 100);
 
-  function hasAllTheImagesBeenLoaded() {
-    for (let image of images) {
-      if (!image.complete) return false;
-    }
-    return true;
+  // As the interval works, make all the images disabled :3
+  for (let image of images) {
+    image.style.pointerEvents = `none`;
   }
-
 }
