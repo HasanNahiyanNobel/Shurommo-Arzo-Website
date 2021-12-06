10be00421c5a1c7a7da2103f37687fed8d140259
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-06 15:21:18 +0600

    Made ids of elements more homogenous. Called additional JavaScripts inside separate functions.

--- src_html/js/bootstrap-5-plus-plus.js
+++ src_html/js/bootstrap-5-plus-plus.js
@@ -1,20 +1,25 @@
-let images = document.getElementsByTagName(`img`);
-let unloadedDivs = document.getElementsByClassName(`oteeddho-unloaded-div`);
-let spinner = document.getElementById(`oteeddho-spinner`);
+startMain();
 
-let interval = setInterval(() => {
-  if (hasAllTheImagesBeenLoaded()) {
-    for (let div of unloadedDivs) {
-      div.classList.remove(`oteeddho-unloaded-div`);
+function startMain() {
+  let images = document.getElementsByTagName(`img`);
+  let unloadedDivs = document.getElementsByClassName(`oteeddho-unloaded-div`);
+  let spinner = document.getElementById(`oteeddho-spinner`);
+
+  let interval = setInterval(() => {
+    if (hasAllTheImagesBeenLoaded()) {
+      for (let div of unloadedDivs) {
+        div.classList.remove(`oteeddho-unloaded-div`);
+      }
+      spinner.classList.add(`oteeddho-unloaded-div`);
+      clearInterval(interval);
     }
-    spinner.classList.add(`oteeddho-unloaded-div`);
-    clearInterval(interval);
-  }
-}, 100);
+  }, 100);
 
-function hasAllTheImagesBeenLoaded() {
-  for (let image of images) {
-    if (!image.complete) return false;
+  function hasAllTheImagesBeenLoaded() {
+    for (let image of images) {
+      if (!image.complete) return false;
+    }
+    return true;
   }
-  return true;
+
 }
--- src_html/js/story-1.js
+++ src_html/js/story-1.js
@@ -1,28 +1,33 @@
-let body = document.body;
-let navbar = document.getElementsByTagName(`nav`)[0];
-let graphics = document.getElementById(`graphics-main-shahorik`);
-let navbarHeight = navbar.offsetHeight;
-let vh = Math.max(document.documentElement.clientHeight || 0,
-    window.innerHeight || 0); // Taken from: https://stackoverflow.com/a/8876069
+startStory1();
 
-window.onscroll = function() {
-  let graphicsPos = graphics.getBoundingClientRect();
-  let currentTop = Math.round(graphicsPos.top) - navbarHeight;
-  let navbarTransitionTime = `1000ms`;
-  let bodyTransitionTime = `5000ms`;
+function startStory1() {
+  let body = document.body;
+  let navbar = document.getElementsByTagName(`nav`)[0];
+  let graphics = document.getElementById(`1-graphics`);
+  let navbarHeight = navbar.offsetHeight;
+  let vh = Math.max(document.documentElement.clientHeight || 0,
+      window.innerHeight || 0); // Taken from: https://stackoverflow.com/a/8876069
 
-  // Add transitions
-  navbar.style.transition = `background-color ${navbarTransitionTime} linear`;
-  body.style.transition = `background-color ${bodyTransitionTime} linear`;
+  window.onscroll = function() {
+    let graphicsPos = graphics.getBoundingClientRect();
+    let currentTop = Math.round(graphicsPos.top) - navbarHeight;
+    let navbarTransitionTime = `1000ms`;
+    let bodyTransitionTime = `5000ms`;
 
-  // When the image comes halfway through the viewport, we see the change of colour
-  if (currentTop < vh / 2) {
-    navbar.classList.remove(`bg-dark`);
-    navbar.style.backgroundColor = `#fbc02d`; // yellow darken-2
-    body.style.backgroundColor = `#fff9c4`; // yellow lighten-4
-  } else {
-    body.style.backgroundColor = ``;
-    navbar.style.backgroundColor = ``;
-    navbar.classList.add(`bg-dark`);
-  }
-};
+    // Add transitions
+    navbar.style.transition = `background-color ${navbarTransitionTime} linear`;
+    body.style.transition = `background-color ${bodyTransitionTime} linear`;
+
+    // When the image comes halfway through the viewport, we see the change of colour
+    if (currentTop < vh / 2) {
+      navbar.classList.remove(`bg-dark`);
+      navbar.style.backgroundColor = `#fbc02d`; // yellow darken-2
+      body.style.backgroundColor = `#fff9c4`; // yellow lighten-4
+    } else {
+      body.style.backgroundColor = ``;
+      navbar.style.backgroundColor = ``;
+      navbar.classList.add(`bg-dark`);
+    }
+  };
+
+}
