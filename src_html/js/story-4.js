14f7424dfc23f5c89cf4e8f6b2c7756bb9f6b949
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-06 17:06:09 +0600

    Transition now starts immediately as user starts to scroll. Changed background colour and the speed of transition.

--- src_html/js/story-4.js
+++ src_html/js/story-4.js
@@ -4,26 +4,19 @@ function startStory4() {
   // Define variables
   let navbar = document.getElementsByTagName(`nav`)[0];
   let body = document.body;
-  let graphics = document.getElementById(`4-graphics`);
-  let navbarHeight = navbar.offsetHeight;
-  let navbarTransitionTime = `3000ms`;
+  let navbarTransitionTime = `4000ms`;
   let bodyTransitionTime = navbarTransitionTime;
-  let changedBackgroundColour = `#17222d`;
+  let transitionSpeedCurve = ``; // `ease` by default
+  let changedBackgroundColour = `#19222c`;
 
   // Add transitions
-  navbar.style.transition = `background-color ${navbarTransitionTime} linear`;
-  body.style.transition = `background-color ${bodyTransitionTime} linear, color ${bodyTransitionTime} linear`;
+  navbar.style.transition = `background-color ${navbarTransitionTime} ${transitionSpeedCurve}`;
+  body.style.transition = `background-color ${bodyTransitionTime} ${transitionSpeedCurve}, color ${bodyTransitionTime} ${transitionSpeedCurve}`;
 
   window.onscroll = function() {
-    let graphicsPos = graphics.getBoundingClientRect();
-    let currentTop = Math.round(graphicsPos.top) - navbarHeight;
-
-    // When image touches the navbar, initiate the change of colour
-    if (currentTop < 0) {
-      navbar.classList.remove(`bg-dark`);
-      navbar.style.backgroundColor = changedBackgroundColour;
-      body.style.backgroundColor = changedBackgroundColour;
-      body.style.color = `#fff`;
-    }
+    navbar.classList.remove(`bg-dark`);
+    navbar.style.backgroundColor = changedBackgroundColour;
+    body.style.backgroundColor = changedBackgroundColour;
+    body.style.color = `#fff`;
   };
 }
