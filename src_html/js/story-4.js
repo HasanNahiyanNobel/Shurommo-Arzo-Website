2bfee0106ca813b27846d444af812a1ffbf9d373
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-06 16:57:25 +0600

    Change of the colour is now initiated as the image touches the navbar.

--- src_html/js/story-4.js
+++ src_html/js/story-4.js
@@ -1,5 +1,29 @@
 startStory4();
 
 function startStory4() {
+  // Define variables
+  let navbar = document.getElementsByTagName(`nav`)[0];
+  let body = document.body;
+  let graphics = document.getElementById(`4-graphics`);
+  let navbarHeight = navbar.offsetHeight;
+  let navbarTransitionTime = `3000ms`;
+  let bodyTransitionTime = navbarTransitionTime;
+  let changedBackgroundColour = `#17222d`;
 
+  // Add transitions
+  navbar.style.transition = `background-color ${navbarTransitionTime} linear`;
+  body.style.transition = `background-color ${bodyTransitionTime} linear, color ${bodyTransitionTime} linear`;
+
+  window.onscroll = function() {
+    let graphicsPos = graphics.getBoundingClientRect();
+    let currentTop = Math.round(graphicsPos.top) - navbarHeight;
+
+    // When image touches the navbar, initiate the change of colour
+    if (currentTop < 0) {
+      navbar.classList.remove(`bg-dark`);
+      navbar.style.backgroundColor = changedBackgroundColour;
+      body.style.backgroundColor = changedBackgroundColour;
+      body.style.color = `#fff`;
+    }
+  };
 }
