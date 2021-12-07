dc7ced149ec3bfc6ade8afb09094ce97b1db9c26
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-07 18:02:20 +0600

    Pre-death paragraphs are now more artistic. 🐸

--- src/js/story-2.js
+++ src/js/story-2.js
@@ -6,20 +6,49 @@ function startStory2() {
 
   function processPreDeathParagraphs() {
     // Get pre-death paragraphs and convert to an array
-    let preDeathParagraphs = Array.from(
-        document.getElementById(`2-pdp`).children,
+    let preDeathParagraphsRegular = Array.from(
+        document.getElementById(`2-pdp-r`).children,
     );
-    let numberOfPreDeathParagraphs = preDeathParagraphs.length;
+    let preDeathParagraphsSpecial = Array.from(
+        document.getElementById(`2-pdp-s`).children,
+    );
 
     // Calculate how much should  the colour component be incremented in each iteration
+    let numberOfPreDeathParagraphsRegular = preDeathParagraphsRegular.length;
+    let numberOfPreDeathParagraphsSpecial = preDeathParagraphsSpecial.length;
+    let numberOfPreDeathParagraphs = numberOfPreDeathParagraphsRegular +
+        numberOfPreDeathParagraphsSpecial;
     let currentShade = 0; // R, G and B—each one gets the same shade component
     let highestShade = 245; // R, G and B—each one gets the same shade component
     let incrementOfShadeInEachIteration = highestShade /
         (numberOfPreDeathParagraphs - 1);
 
-    // Modify the pre-death paragraphs
-    preDeathParagraphs.forEach((item, index) => {
+    // Modify the regular pre-death paragraphs
+    preDeathParagraphsRegular.forEach((item, index) => {
       item.style.marginLeft = `${3 * index}%`;
+      item.style.color = `rgb(${currentShade}, ${currentShade}, ${currentShade})`;
+      currentShade += incrementOfShadeInEachIteration;
+    });
+
+    // Modify the special pre-death paragraphs
+    preDeathParagraphsSpecial.forEach((item, index) => {
+      switch (index) {
+        case 0: // অমল আলো জ্বালবে না...
+          item.style.marginLeft = `${3 *
+          (index + numberOfPreDeathParagraphsRegular) - 19.5}%`;
+          break;
+        case 1: // অমল তার স্বপ্ন ও বাস্তবতার ঘোর নিয়ে...
+          item.style.marginLeft = `${3 *
+          (index + numberOfPreDeathParagraphsRegular) - 12.5}%`;
+          break;
+        case 2: // একটা অন্তহীন ঘুমে সে ডুবে ডুবে...
+          item.style.transform = `rotate(180deg)`;
+          break;
+        default: // জিম মরিসনের মতো একটা ব্যাখ্যাতীত...
+          item.style.marginLeft = `${3 *
+          (index + numberOfPreDeathParagraphsRegular) - 8.5}%`;
+          break;
+      }
       item.style.color = `rgb(${currentShade}, ${currentShade}, ${currentShade})`;
       currentShade += incrementOfShadeInEachIteration;
     });
