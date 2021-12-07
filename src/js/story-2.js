5db06703221fd6552478124b49310d978c505635
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-07 16:35:26 +0600

    Quarter way through implementing story 2.

---
+++ src/js/story-2.js
@@ -0,0 +1,9 @@
+startStory2();
+
+function startStory2() {
+  let preDeathParagraphs = document.getElementById(`2-d`).children;
+
+  for (let preDeathParagraph of preDeathParagraphs) {
+    console.log(preDeathParagraph);
+  }
+}
