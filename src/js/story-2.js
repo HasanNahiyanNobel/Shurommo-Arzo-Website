22be59f7ed8956e94d97abadf521159cad1937fb
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-07 16:57:56 +0600

    Created JS to shade and give margins to the pre death paragraphs.

--- src/js/story-2.js
+++ src/js/story-2.js
@@ -1,9 +1,22 @@
 startStory2();
 
 function startStory2() {
-  let preDeathParagraphs = document.getElementById(`2-d`).children;
+  // Get pre-death paragraphs and convert to an array
+  let preDeathParagraphs = Array.from(
+      document.getElementById(`2-pdp`).children,
+  );
+  let numberOfPreDeathParagraphs = preDeathParagraphs.length;
 
-  for (let preDeathParagraph of preDeathParagraphs) {
-    console.log(preDeathParagraph);
-  }
+  // Calculate how much should  the colour component be incremented in each iteration
+  let currentShade = 0; // R, G and B—each one gets the same shade component
+  let highestShade = 245; // R, G and B—each one gets the same shade component
+  let incrementOfShadeInEachIteration = highestShade /
+      (numberOfPreDeathParagraphs - 1);
+
+  // Modify the pre-death paragraphs
+  preDeathParagraphs.forEach((item, index) => {
+    item.style.marginLeft = `${3 * index}%`;
+    item.style.color = `rgb(${currentShade}, ${currentShade}, ${currentShade})`;
+    currentShade += incrementOfShadeInEachIteration;
+  });
 }
