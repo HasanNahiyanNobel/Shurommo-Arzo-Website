52b07f7c4675e2968f22939ca1402dee443eddc3
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-12-21 00:48:37 +0600

    Added the tube-light effect.

--- src/js/story-5.js
+++ src/js/story-5.js
@@ -3,15 +3,18 @@ startStory5();
 function startStory5() {
   // Get the style of the paragraph reading `এই ছিলো, এই নাই`
   let ecenStyle = document.getElementById(`5-ecen`).style;
+  let probabilityOfVisibilitySwitch = 0.2;
+  let intervalTimeOut = 200;
 
-  setTimeout(() => {
-    switchVisibilityOfEcen();
-  }, 2000);
+  // Set the interval for tube-light effect
+  setInterval(() => {
+    let theRandom = Math.random();
+    if (theRandom < probabilityOfVisibilitySwitch) {
+      switchVisibilityOfEcen();
+    }
+  }, intervalTimeOut);
 
-  setTimeout(() => {
-    switchVisibilityOfEcen();
-  }, 3100);
-
+  // Function for switching the visibility of the desired paragraph
   function switchVisibilityOfEcen() {
     let currentVisibility = ecenStyle.visibility;
     ecenStyle.visibility = currentVisibility === `` ? `hidden` : ``;
