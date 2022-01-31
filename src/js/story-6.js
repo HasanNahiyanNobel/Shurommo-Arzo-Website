55d6c63df1b062c80659b53feb825a92b04a08a2
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-02-01 03:23:47 +0600

    Started the story from bold font. Couldn't be satisfied with this.

--- src/js/story-6.js
+++ src/js/story-6.js
@@ -1,22 +1,36 @@
 startStory6();
 
 function startStory6() {
+  // Get document elements
+  let firstParas = document.getElementById(`6-f`);
+  let lastPara = document.getElementById(`6-l`);
   let mainPara = document.getElementById(`6-m`);
+
+  // Process the main para
   let mainParaAsArray = mainPara.innerText.split(` `);
   let mainParaLength = mainParaAsArray.length;
 
+  // Define pseudo-constants
   let maxFontWeight = 999;
-  let minFontWeight = 300;
+  let minFontWeight = 200;
 
-  let fontWeightIncrement = (maxFontWeight - minFontWeight) /
+  // Calculate the decrement of font weight
+  let fontWeightDecrement = (maxFontWeight - minFontWeight) /
       (mainParaLength - 1);
 
+  // Add weights to the words of the main paragraph
   mainParaAsArray = mainParaAsArray.map((item, index) => {
-    let fontWeight = minFontWeight + fontWeightIncrement *
-        (mainParaLength - index - 1);
-    console.log(fontWeight);
-    return `<span style="font-weight: ${fontWeight}">` + item + `</span>`;
+    let fontWeight = maxFontWeight - fontWeightDecrement * index;
+    return `<span style="font-variation-settings: 'wght' ${fontWeight}">` +
+        item + `</span>`;
   });
 
+  // Set the weight-added array as the inner HTML of the main paragraph
   mainPara.innerHTML = mainParaAsArray.join(` `);
+
+  // Add weight to the first paragraph
+  firstParas.style.fontVariationSettings = `'wght' ${maxFontWeight}`;
+
+  // Add weight to the last paragraph
+  lastPara.style.fontVariationSettings = `'wght' ${minFontWeight}`;
 }
