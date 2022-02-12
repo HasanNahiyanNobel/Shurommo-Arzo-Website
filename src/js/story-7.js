b7445f536862f7dc421a9332d070d2c04f66906d
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-02-12 23:45:18 +0600

    Changed some timings. Colour changes feel more subtle and sane now.

--- src/js/story-7.js
+++ src/js/story-7.js
@@ -7,43 +7,41 @@ function startStory7() {
     // document.getElementById(`7-mt`).click(); // TODO: Uncomment this
   }, 150);
 
-
   // Define pseudo-constants
-  let bgColor = `#e8e0d2`;
-  let transitionTime = `2s`;
+  let bgColourLight = `#ffffff`;
+  let bgColourDark = `#e8e0d2`;
+  let transitionTime = `1500ms`;
   let transitionSpeedCurve = `linear`; // `ease` by default
   let isBGColoured = false;
-
+  let probabilityOfColourChange = 0.75;
 
   // Get document elements
   let body = document.body;
   let hiddenSpan = document.getElementById(`8-ht`);
 
-
   // Ensure that the background and hidden text has the same colours
-  body.style.backgroundColor = `#fff`;
-  hiddenSpan.style.color = `#fff`;
-
+  body.style.backgroundColor = bgColourLight;
+  hiddenSpan.style.color = bgColourLight;
 
   // Add transitions
   body.style.transition = `background-color ${transitionTime} ${transitionSpeedCurve}`;
   hiddenSpan.style.transition = `color ${transitionTime} ${transitionSpeedCurve}`;
 
-
   // Change the colours
   setInterval(() => {
-    switchColours();
-  }, 2000);
-
+    if (Math.random() < probabilityOfColourChange) {
+      switchColours();
+    }
+  }, 10000);
 
   // Function to switch the colours
   function switchColours() {
     if (isBGColoured) {
-      body.style.backgroundColor = `#fff`;
-      hiddenSpan.style.color = `#fff`;
+      body.style.backgroundColor = bgColourLight;
+      hiddenSpan.style.color = bgColourLight;
     } else {
-      body.style.backgroundColor = bgColor;
-      hiddenSpan.style.color = bgColor;
+      body.style.backgroundColor = bgColourDark;
+      hiddenSpan.style.color = bgColourDark;
     }
     isBGColoured = !isBGColoured;
   }
