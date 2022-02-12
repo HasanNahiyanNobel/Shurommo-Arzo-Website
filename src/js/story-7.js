cb39e1558dedd9b12727d80911e40dceda37e038
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-02-12 22:16:42 +0600

    Created the basic layout.

---
+++ src/js/story-7.js
@@ -0,0 +1,8 @@
+startStory7();
+
+function startStory7() {
+  // Trigger the modal of info
+  setTimeout(() => {
+    // document.getElementById(`7-mt`).click(); // TODO: Uncomment this
+  }, 150);
+}
