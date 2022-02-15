74340e8edfaae5cd0421e79d18b7006f33f03ae0
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-02-16 02:29:40 +0600

    Generated meta tags for the story Shahorik.

--- converter_beta.py
+++ converter_beta.py
@@ -12,23 +12,20 @@ import glob
 import json
 
 # Variables
-pre_title = []
+pre_meta_tags = []
 meta_tags = []
-pre_description = []
-description = []
 pre_content = []
 content = []
 post_content = []
 
-# TODO: Remove the following comment
-# meta_title = ''
-# meta_description = ''
-# meta_image = ''
-# meta_author = ''
-# meta_og_type = ''
-# meta_url = ''
-# meta_twitter_domain = ''
-# meta_twitter_card = ''
+meta_title = ''
+meta_description = ''
+meta_image = ''
+meta_author = ''
+meta_og_type = ''
+meta_url = ''
+meta_twitter_domain = ''
+meta_twitter_card = ''
 
 # Format every .py file using yapf Google style
 py_files = glob.glob('*.py')
@@ -38,14 +35,9 @@ for py_file in py_files:
 # Read pre- and post-contents from base file
 with open(BASE_FILE, encoding='utf-8') as f:
     for line in f:
-        if line.strip() == TITLE_BLOCK_MARKER:
+        if line.strip() == META_BLOCK_MARKER:
             break
-        pre_title.append(line)
-
-    for line in f:
-        if line.strip() == DESCRIPTION_BLOCK_MARKER:
-            break
-        pre_description.append(line)
+        pre_meta_tags.append(line)
 
     for line in f:
         if line.strip() == CONTENT_BLOCK_MARKER:
@@ -73,52 +65,44 @@ for source_file in source_files:
                 meta_tags = read_block(f)
                 meta_tags = ''.join(meta_tags)
                 meta_tags = json.loads(meta_tags)
-                if 'title' in meta_tags:
-                    meta_title = meta_tags['title']
-                else:
-                    meta_title = TITLE_DEFAULT
-                if 'description' in meta_tags:
-                    meta_description = meta_tags['description']
-                else:
-                    meta_description = DESCRIPTION_DEFAULT
-                if 'image' in meta_tags:
-                    meta_image = meta_tags['image']
-                else:
-                    meta_image = IMAGE_DEFAULT
-                if 'author' in meta_tags:
-                    meta_author = meta_tags['author']
-                else:
-                    meta_author = AUTHOR_DEFAULT
-                if 'og_type' in meta_tags:
-                    meta_og_type = meta_tags['og_type']
-                else:
-                    meta_og_type = OG_TYPE_DEFAULT
-                if 'url' in meta_tags:
-                    meta_url = meta_tags['url']
-                else:
-                    meta_url = URL_DEFAULT
-                if 'twitter_domain' in meta_tags:
-                    meta_twitter_domain = meta_tags['twitter_domain']
-                else:
-                    meta_twitter_domain = TWITTER_DOMAIN_DEFAULT
-                if 'twitter_card' in meta_tags:
-                    meta_twitter_card = meta_tags['twitter_card']
-                else:
-                    meta_twitter_card = TWITTER_CARD_DEFAULT
-                print(meta_title + '\n' + meta_description + '\n' + meta_image +
-                      '\n' + meta_author + '\n' + meta_og_type + '\n' +
-                      meta_url + '\n' + meta_twitter_domain + '\n' +
-                      meta_twitter_card)
+
+                meta_title = set_title(meta_tags)
+                meta_description = set_description(meta_tags)
+                meta_image = set_image(meta_tags)
+                meta_author = set_author(meta_tags)
+                meta_og_type = set_og_type(meta_tags)
+                meta_url = set_url(meta_tags)
+                meta_twitter_domain = set_twitter_domain(meta_tags)
+                meta_twitter_card = set_twitter_cards(meta_tags)
+
+                print(''.join(meta_title))
+                print(''.join(meta_description))
+                print(''.join(meta_image))
+                print(''.join(meta_author))
+                print(''.join(meta_og_type))
+                print(''.join(meta_url))
+                print(''.join(meta_twitter_domain))
+                print(''.join(meta_twitter_card))
+
+                meta_tags = meta_title
+                meta_tags += meta_description
+                meta_tags += meta_image
+                meta_tags += meta_author
+                meta_tags += meta_og_type
+                meta_tags += meta_url
+                meta_tags += meta_twitter_domain
+                meta_tags += meta_twitter_card
+
             if current_line == START_OF_CONTENT_BLOCK_MARKER:
                 content = read_block(f, True)
 
     # Write the contents in respective output files
-    # output_path = OUTPUT_DIR + '\\' + source_file
-    # with open(
-    #         output_path, 'w+', encoding='utf-8'
-    # ) as f:  # The 'w+' option creates the file if it does not exist already
-    #     f.writelines(pre_title + meta_tags + pre_description + description +
-    #                  pre_content + content + post_content)
+    output_path = OUTPUT_DIR + '\\' + source_file
+    with open(
+            output_path, 'w+', encoding='utf-8'
+    ) as f:  # The 'w+' option creates the file if it does not exist already
+        f.writelines(pre_meta_tags + meta_tags + pre_content + content +
+                     post_content)
 
     # Clean the arrays
     fluid_content = []
--- converter_utilities.py
+++ converter_utilities.py
@@ -90,3 +90,95 @@ def english_to_bangla_number(number=None):
         return ''
     except TypeError:
         return ''
+
+
+def set_title(tags):
+    if 'title' in tags:
+        title = tags['title']
+        title_meta = ['<title>', title, ' – ', TITLE_DEFAULT, '</title>']
+    else:
+        title = TITLE_DEFAULT
+        title_meta = ['<title>', title, '</title>']
+    title_meta += [
+        '<meta property="og:title" content="', title, '">',
+        '<meta name="twitter:title" content="', title, '">'
+    ]
+    return title_meta
+
+
+def set_description(tags):
+    if 'description' in tags:
+        description_meta = tags['description']
+    else:
+        description_meta = DESCRIPTION_DEFAULT
+    description_meta = [
+        '<meta name="description" content="', description_meta, '...">',
+        '<meta property="og:description" content="', description_meta, '...">',
+        '<meta name="twitter:description" content="', description_meta, '...">'
+    ]
+    return description_meta
+
+
+def set_image(tags):
+    if 'image' in tags:
+        image_meta = tags['image']
+    else:
+        image_meta = IMAGE_DEFAULT
+    image_meta = [
+        '<meta property="og:image" content="', image_meta, '">',
+        '<meta name="twitter:image" content="', image_meta, '">',
+        '<meta name="image" property="og:image" content="', image_meta, '">'
+    ]  # The last one is for LinkedIn
+    return image_meta
+
+
+def set_author(tags):
+    if 'author' in tags:
+        author_meta = tags['author']
+    else:
+        author_meta = AUTHOR_DEFAULT
+    author_meta = ['<meta name="author" content="', author_meta, '">']
+    return author_meta
+
+
+def set_og_type(tags):
+    if 'og_type' in tags:
+        og_type_meta = tags['og_type']
+    else:
+        og_type_meta = OG_TYPE_DEFAULT
+    og_type_meta = ['<meta property="og:type" content="', og_type_meta, '">']
+    return og_type_meta
+
+
+def set_url(tags):
+    if 'url' in tags:
+        url_meta = tags['url']
+    else:
+        url_meta = URL_DEFAULT
+    url_meta = [
+        '<meta property="og:url" content="', url_meta, '">',
+        '<meta property="twitter:url" content="', url_meta, '">'
+    ]
+    return url_meta
+
+
+def set_twitter_domain(tags):
+    if 'twitter_domain' in tags:
+        twitter_domain_meta = tags['twitter_domain']
+    else:
+        twitter_domain_meta = TWITTER_DOMAIN_DEFAULT
+    twitter_domain_meta = [
+        '<meta property="twitter:domain" content="', twitter_domain_meta, '">'
+    ]
+    return twitter_domain_meta
+
+
+def set_twitter_cards(tags):
+    if 'twitter_card' in tags:
+        twitter_card_meta = tags['twitter_card']
+    else:
+        twitter_card_meta = TWITTER_CARD_DEFAULT
+    twitter_card_meta = [
+        '<meta name="twitter:card" content="', twitter_card_meta, '">'
+    ]
+    return twitter_card_meta
