b969433308bd1911f5d22d55aa14f1be9466341e
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-03-16 00:07:49 +0600

    Added a basic JS for story 8.

---
+++ src/js/story-8.js
@@ -0,0 +1,15 @@
+startStory8();
+
+function startStory8() {
+  // Trigger the modal of info
+  setTimeout(() => {
+    // document.getElementById(`8-mt`).click(); // TODO: Activate this later
+  }, 150);
+
+  // Get document elements
+  let body = document.body;
+
+  body.classList.add(`bg-dark`);
+  body.classList.add(`text-light`);
+
+}
