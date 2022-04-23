7d5ca82f18c4084afe8d8dca82724a3a05155d2b
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-04-23 20:59:57 +0600

    Tried to draw circle using svg tag.

--- src/js/beta.js
+++ src/js/beta.js
@@ -1,22 +1,49 @@
 startBeta();
 
 function startBeta() {
-  let testDiv = document.getElementById(`test-div`);
+  drawCircles();
+  drawAnimatedRectangle();
 
-  let transitionTime = 1000;
-  let height = 50;
-  let increment = 50;
-  let maxHeight = 500;
+  function drawCircles() {
+    let circleDiv = document.getElementById(`beta-circle-div`);
 
-  testDiv.style.height = `${height}px`;
-  testDiv.style.backgroundColor = `#222`;
-  testDiv.style.transition = `height ${transitionTime}ms linear`;
+    let width = 64;
+    let colour1 = `rgba(0, 133, 176, 0.5)`;
+    let colour2 = `rgba(176, 0, 0, 0.5)`;
 
-  setTimeout(callback, 200);
-  setInterval(callback, transitionTime);
+    let circle1 = getCircleAsString(width, width, width, colour1);
+    let circle2 = getCircleAsString(width - width / 2, width, width, colour2);
 
-  function callback() {
-    height = (height + increment) % maxHeight;
+    circleDiv.innerHTML += circle1;
+    circleDiv.innerHTML += circle2;
+
+    function getCircleAsString(cx, cy, radius, colour) {
+      return `<svg xmlns="http://www.w3.org/2000/svg" 
+                width="${2 * radius}" height="${2 * radius}"
+                class="bi bi-circle-fill">
+                <circle cx="${cx}" cy="${cy}" r="${radius}" fill="${colour}"/>
+              </svg>`;
+    }
+  }
+
+  function drawAnimatedRectangle() {
+    let testDiv = document.getElementById(`beta-test-div`);
+
+    let transitionTime = 1000;
+    let height = 50;
+    let increment = 50;
+    let maxHeight = 500;
+
     testDiv.style.height = `${height}px`;
+    testDiv.style.backgroundColor = `#222`;
+    testDiv.style.transition = `height ${transitionTime}ms linear`;
+
+    setTimeout(callback, 200);
+    setInterval(callback, transitionTime);
+
+    function callback() {
+      height = (height + increment) % maxHeight;
+      testDiv.style.height = `${height}px`;
+    }
   }
 }
