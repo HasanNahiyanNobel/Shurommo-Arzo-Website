10c5809e2766928b61ece655d53b12680832ed28
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-04-30 19:25:06 +0600

    Created the initial layout.

---
+++ src/js/story-10.js
@@ -0,0 +1,8 @@
+startStory10();
+
+function startStory10() {
+  // Trigger the modal of info
+  setTimeout(() => {
+    // document.getElementById(`10-mt`).click(); // TODO: Activate this
+  }, 150);
+}
