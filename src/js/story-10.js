2fc3ea6bdabb0e74b0665577b60c788e06a09918
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-04-30 23:02:36 +0600

    First two images are now synced with the first two notes of the solo.

--- src/js/story-10.js
+++ src/js/story-10.js
@@ -8,6 +8,10 @@ function startStory10() {
     // document.getElementById(`10-mt`).click(); // TODO: Activate this
   }, 150);
 
+  // Define extensions as variable
+  let mp3Extension = `.mp3`;
+  let svgExtension = `.svg`;
+
   // Get document elements
   let modalButton = document.getElementById(`10-y`);
   let fadeInLines = Array.from(document.getElementsByClassName(`10-fil`));
@@ -18,6 +22,8 @@ function startStory10() {
   let divOfSoloAndGraphics = document.getElementById(`10-sag`);
 
   // Specify pseudo-constants
+  let audioSrc = `audios/`;
+  let imageSrc = `images/`;
   let colourLight = `#ffffff`;
   let colourDark = `#212529`; // Bootstrap dark colour
   let transitionTimeInMS = 500; // TODO: Make this 2000
@@ -25,6 +31,10 @@ function startStory10() {
   let appearanceTimeOfLine1 = 500;
   let appearanceTimeOfLine2 = appearanceTimeOfLine1 + transitionTimeInMS;
   let startOfSoloAndGraphics = appearanceTimeOfLine2 + transitionTimeInMS;
+
+  // Calculate other derivative variables
+  let soloPath = audioSrc + `story-10` + mp3Extension;
+  let solo = new Audio(soloPath);
 
   // Process the fade-in lines
   fadeInLines.forEach(line => {
@@ -47,6 +57,14 @@ function startStory10() {
     // Remove the event listener
     modalButton.removeEventListener(`click`, readerIsOkay);
 
+    // When the solo has been loaded upto a substantial amount, start showing the story
+    solo.addEventListener(`canplaythrough`, postAudioLoadRoutine);
+  }
+
+  function postAudioLoadRoutine() {
+    // First remove the listener
+    solo.removeEventListener(`canplaythrough`, postAudioLoadRoutine);
+
     // Fade in the first two lines
     setTimeout(() => {
       line1.style.color = colourDark;
@@ -62,9 +80,19 @@ function startStory10() {
   }
 
   function playSoloAndShowGraphics() {
-    // Show the images
-    fadeInGraphicsOfSolo.forEach(graphic => {
-      graphic.style.opacity = `1`;
+    // Play audio
+    solo.play().then(() => {
+      showGraphics();
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  function showGraphics() {
+    fadeInGraphicsOfSolo.forEach((graphic, index) => {
+      setTimeout(() => {
+        graphic.style.opacity = `1`;
+      }, (4 * index + 1.2) * 1000); // 1.2 instead of 1—to delay the graphics by a suitable fraction
     });
   }
 }
