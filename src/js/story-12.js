a19763a7bf4d0e9623e446889fd89f82f7b26c3c
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-10-03 03:09:54 +0600

    The first audio is played properly.

--- src/js/story-12.js
+++ src/js/story-12.js
@@ -7,19 +7,45 @@ function startStory12() {
   // Get the document elements
   let spinner = document.getElementById(`ms`); // Main spinner from the base template
   let modalButton = document.getElementById(`12-y`);
+  let title = document.getElementsByTagName(`h1`)[0];
+  let part1Div = document.getElementById(`12-p-1`);
+  let part2Div = document.getElementById(`12-p-2`);
+  let documentDivs = [
+    title,
+    part1Div,
+    part2Div,
+  ];
 
   // Define pseudo-constants
   let audioSrc = `audios/`;
   let audioPrefix = `story-12-`;
+  let audio1;
+  let audio2;
+  let colourLight = `#ffffff`;
+  let colourDark = `#212529`; // Bootstrap dark colour
+  let transitionTimeInMSForTitle = 2000;
+  let transitionTimeInMSForPart1Div = 3000;
+  let transitionSpeedCurve = `linear`;
+  let titleDisplayTimeout = 100; // The element is displayed this ms after the audio is played
+  let part1DivDisplayTimeout = 10e3;
 
+  processDocumentDivs();
   loadAudios();
   giveRandomMarginToTheSelectedParagraphs();
+
+  function processDocumentDivs() {
+    documentDivs.forEach(div => {
+      div.style.color = colourLight;
+    });
+    title.style.transition = `color ${transitionTimeInMSForTitle}ms ${transitionSpeedCurve}`;
+    part1Div.style.transition = `color ${transitionTimeInMSForPart1Div}ms ${transitionSpeedCurve}`;
+  }
 
   function loadAudios() {
     let audio1Path = audioSrc + audioPrefix + `1` + mp3Extension;
     let audio2Path = audioSrc + audioPrefix + `2` + mp3Extension;
-    let audio1 = new Audio(audio1Path);
-    let audio2 = new Audio(audio2Path);
+    audio1 = new Audio(audio1Path);
+    audio2 = new Audio(audio2Path);
 
     // Show the spinner until the scream has been loaded.
     // However, the central JS for this website removes the spinner **after** the
@@ -46,6 +72,12 @@ function startStory12() {
         // Remove the spinner again!
         clearInterval(forceSpinnerForAudio); // Clear the scheduled interval
         spinner.classList.add(`d-none`);
+        {
+          // Sometimes the spinner is still visible. Adding this classes as a quick fix
+          // TODO: Find out the root of this problem and debug this.
+          spinner.classList.add(`d-none`);
+          spinner.classList.add(`d-none`);
+        }
       }
     }
 
@@ -65,6 +97,22 @@ function startStory12() {
   function readerIsOkay() {
     // Remove the event listener
     modalButton.removeEventListener(`click`, readerIsOkay);
+
+    // Play the first audio
+    audio1.play().then(() => {
+      title.classList.remove(`d-none`);
+      part1Div.classList.remove(`d-none`);
+
+      // Display the title
+      setTimeout(() => {
+        title.style.color = colourDark;
+      }, titleDisplayTimeout);
+
+      // Display part 1 div
+      setTimeout(() => {
+        part1Div.style.color = colourDark;
+      }, part1DivDisplayTimeout);
+    });
   }
 
   function giveRandomMarginToTheSelectedParagraphs() {
