0f5087af818f839869c5590b7141f03ed8a92042
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-10-03 01:50:25 +0600

    Gave some paragraphs random left margin.

--- src/js/story-12.js
+++ src/js/story-12.js
@@ -1,5 +1,17 @@
 startStory12();
 
 function startStory12() {
+  let randomMarginLeftParagraphs = Array.from(
+      document.getElementsByClassName(`12-rmlp`));
+  /**
+   * The paragraphs having random margins are already inside a div of 7.5% margin.
+   * So the if we want the largest margin to be x%, we have to set the variable
+   * to x-7.5.
+   */
+  let largestRandomMargin = 30;
 
+  randomMarginLeftParagraphs.forEach(para => {
+    let randomLeftMargin = Math.random() * largestRandomMargin;
+    para.style.marginLeft = `${randomLeftMargin}%`;
+  });
 }
