3e48b308705bb6ee8411fac2ae667c584eda6fa6
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-10-03 06:09:21 +0600

    Removed the concept of a div of space. Calibrated timeouts for part 2.

--- src/js/story-12.js
+++ src/js/story-12.js
@@ -8,7 +8,6 @@ function startStory12() {
   // Get the document elements
   let navbar = document.getElementById(`mn`); // Main navbar
   let spinner = document.getElementById(`ms`); // Main spinner from the base template
-  let divOfSpace = document.getElementById(`12-s`);
   let modalButton = document.getElementById(`12-y`);
   let title = document.getElementsByTagName(`h1`)[0];
   let part1Div = document.getElementById(`12-p-1`);
@@ -29,29 +28,23 @@ function startStory12() {
   let colourDark = `#212529`; // Bootstrap dark colour
   let transitionTimeInMSForTitle = 2e3;
   let transitionTimeInMSForPart1Div = 3e3;
-  let transitionTimeInMSForPart2Divs = 1e3;
+  let transitionTimeInMSForPart2Divs = 2e3;
   let transitionSpeedCurve = `linear`;
   let titleDisplayTimeout = 100; // The element is displayed this ms after the audio is played
   let part1DivDisplayTimeout = 10e3;
   let part2DivDisplayTimeouts = [
-    1e3,
     5.5e3,
     11e3,
     23.3e3,
-    35.5e3,
+    35.4e3,
+    47.2e3,
   ];
-
-  // Get viewport height
-  let vh = Math.max(document.documentElement.clientHeight || 0,
-      window.innerHeight || 0); // Also from: https://stackoverflow.com/a/8876069
 
   processDocumentDivs();
   loadAudios();
   giveRandomMarginToTheSelectedParagraphs();
 
   function processDocumentDivs() {
-    divOfSpace.style.minHeight = `${vh}px`;
-
     documentDivs.forEach(div => {
       div.style.color = colourLight;
     });
@@ -127,16 +120,18 @@ function startStory12() {
       // Remove d-none
       title.classList.remove(`d-none`);
       part1Div.classList.remove(`d-none`);
-      divOfSpace.classList.remove(`d-none`);
 
       // Display the title
       setTimeout(() => {
         title.style.color = colourDark;
       }, titleDisplayTimeout);
 
-      // Display part 1 div and listen to scroll
+      // Display part 1 div, create space for part 2, and listen to scroll
       setTimeout(() => {
+        // Display part 1
         part1Div.style.color = colourDark;
+        // Create space for part 2
+        part2Div.classList.remove(`d-none`);
         // Listen to the scroll, and when the div of space reaches close to the top, play the next audio
         document.addEventListener(`scroll`, scrollListener);
       }, part1DivDisplayTimeout);
@@ -152,13 +147,6 @@ function startStory12() {
     if (hasBottomOfPageReached) {
       document.removeEventListener(`scroll`, scrollListener);
       audio2.play().then(() => {
-
-        // Remove the space
-        divOfSpace.classList.add(`d-none`);
-
-        // Create space for the div of part 2
-        part2Div.classList.remove(`d-none`);
-
         // Set timeouts
         partsOfPart2Div.forEach((part, index) => {
           setTimeout(() => {
